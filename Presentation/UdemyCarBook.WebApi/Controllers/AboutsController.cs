5eb1b5a0828c4f7db7cdb7f2171646c92750792f
bidoluyazilim <112986515+Ertanso@users.noreply.github.com>
2023-12-26 15:10:42 +0300

    commit

---
+++ Presentation/UdemyCarBook.WebApi/Controllers/AboutsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+
+namespace UdemyCarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutsController : ControllerBase
+    {
+        private readonly CreateAboutCommandHandler _createAboutCommandHandler;
+        private readonly Get
+    }
+}
